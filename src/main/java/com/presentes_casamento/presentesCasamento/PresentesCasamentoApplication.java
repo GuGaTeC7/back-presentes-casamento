0865388c3f9028a672fe2d5b2fb561369de93eb3
GuGaTeC7 <gustavoluizsilva245@gmail.com>
2024-11-01 14:52:50 -0300

    docs: arquivos iniciais

---
+++ src/main/java/com/presentes_casamento/presentesCasamento/PresentesCasamentoApplication.java
@@ -0,0 +1,13 @@
+package com.presentes_casamento.presentesCasamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresentesCasamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresentesCasamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/presentes_casamento/presentesCasamento/PresentesCasamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.presentes_casamento.presentesCasamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresentesCasamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
